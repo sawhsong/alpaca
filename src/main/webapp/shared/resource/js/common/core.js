9f61530db11f873a820bf9b3d5bc8979da5e018a
sawhsong <sawhsong@gmail.com>
2023-06-27 09:14:07 +1000

    Signed-off-by: sawhsong <sawhsong@gmail.com>

--- src/main/webapp/shared/resource/js/common/core.js
+++ src/main/webapp/shared/resource/js/common/core.js
@@ -789,7 +789,7 @@ var nony = {
 	},
 	toCamelCase : function(val) {
 		if ($.nony.isBlank(val)) {return "";}
-		if (!$.nony.contains(val, "_")) {return val;}
+		if (!$.nony.contains(val, "_")) {return val.toLowerCase();}
 		return val.toLowerCase().replace(/(\_[a-z])/g, function(arg) {
 			return arg.toUpperCase().replace('_','');
 		});
